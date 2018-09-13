a48cde9663af92b87a82f1ecbb06a74611dbf080
kazuki takao <kazukitakao55@gmail.com>
2018-09-13 23:47:24 +0900

    コメントを追記

--- server.js
+++ server.js
@@ -6,10 +6,27 @@ const line = require('@line/bot-sdk');
 
 const PORT = process.env.PORT || 3000;
 
+// LINESECRETとTOKEN
 const config = {
     channelSecret: 'dda96be6327f93bbbcbd6880821fa397',
     channelAccessToken: 'BYHiPhzvLQBYw/tEMqfHWQyRTSgXMq6hwUVhujkJroYUiDTtvoeLraaEqCDLJdoSpBKNrhBN4vsvqQrYPUwkRMOh6O9SJZsZwDyjnLtcNp4ceWKkviqO2hPnrMUPBuGZLC+2DZ3d42DKXoicx84HzwdB04t89/1O/w1cDnyilFU='
 };
+
+// 使用可能性のあるライブラリ
+// sequeilize DB操作用
+// xmlパース
+// 
+
+
+// データ取得先のURLを設定する
+// XMLパースしてデータを取得する
+// 取得するデータをxpathで指定する
+// 雨降るよちゃんだと時間ごとの降水確率を取得
+// ％の下限値を設定
+// ％によって送信するメッセージを設定する
+// 文字列の中に取得した降水確率をFormatする
+// 送信先ユーザはIDを配列にして渡す必要がある
+
 
 // インスタンス化してappに代入
 const app = express();
@@ -21,6 +38,7 @@ app.post('/webhook',line.middleware(config),(req,res) => {
         .then((result) => res.json(result));
 });
 
+// 
 const client = new line.Client(config);
 
 function handleEvent(event){
@@ -28,6 +46,7 @@ function handleEvent(event){
         return Promise.resolve(null);
     }
 
+    // テキストメッセージリクエストを行う関数
     return client.replyMessage(event.replyToken,{
         type: 'text',
         text: event.message.text // 実際に返信の言葉を入れる箇所
