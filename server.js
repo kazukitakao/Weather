ce111b71b433b3bc468e1f4b490fe856d70cbde3
kazuki takao <kazukitakao55@gmail.com>
2018-09-22 16:45:26 +0900

    取得したrssの情報をJSON形式に整形

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 
 // expressモジュールをロード
 const express = require('express');
+// LINESDK
 const line = require('@line/bot-sdk');
+// XMLパース
 const parseString = require('xml2js').parseString;
 
 const PORT = process.env.PORT || 3000;
@@ -13,24 +15,24 @@ const config = {
     channelAccessToken: 'BYHiPhzvLQBYw/tEMqfHWQyRTSgXMq6hwUVhujkJroYUiDTtvoeLraaEqCDLJdoSpBKNrhBN4vsvqQrYPUwkRMOh6O9SJZsZwDyjnLtcNp4ceWKkviqO2hPnrMUPBuGZLC+2DZ3d42DKXoicx84HzwdB04t89/1O/w1cDnyilFU='
 };
 
-// 使用可能性のあるライブラリ
-// sequeilize DB操作用
+const client = new line.Client(config);
 
+// 3つの関数を作成予定？？
+// rssを取得する関数
+// rssを整形する関数
+// LINEへpushする関数
 
 // データ取得先のURLを設定する
 const url = "http://www.drk7.jp/weather/xml/13.xml";
-// XMLパースしてデータを取得する
+
 let request = require('request');
 
 request(url, function (err, response, body) {
     if (!err && response.statusCode == 200) {
         analyzeRSS(body);
     }
-})
+});
 
-// 取得するデータをxpathで指定する
-// 雨降るよちゃんだと時間ごとの降水確率を取得
-// ％の下限値を設定
 // ％によって送信するメッセージを設定する
 // 文字列の中に取得した降水確率をFormatする
 function analyzeRSS(rss) {
@@ -39,30 +41,30 @@ function analyzeRSS(rss) {
 
         let date = obj.weatherforecast.pref[0].area[3].info[0].$.date;
         let items = obj.weatherforecast.pref[0].area[3].info[0].rainfallchance;
- 
-        console.log('今日の日付：' + date);
+        
+        let message = '今日の日付：' + date + '\n';
         items[0].period.forEach(period => {
-            console.log(period.$.hour + '時：' + period._ + '%');
+            message += period.$.hour + '時：' + period._ + '% \n'
+            //console.log(period.$.hour + '時：' + period._ + '%');
         });
+        console.log(message);
+        // 取得したデータをjson形式に変換
+        console.log(JSON.stringify({
+            type: 'text',
+            text: message
+        }));
     });
 }
 
-
-// 送信先ユーザはIDを配列にして渡す必要がある
-
-
-// インスタンス化してappに代入
 const app = express();
 
 app.post('/webhook', line.middleware(config), (req, res) => {
     console.log(req.body.events);
+    // handleEventが完全に完了したら、json形式で返す
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
 });
-
-// 
-const client = new line.Client(config);
 
 function handleEvent(event) {
     if (event.type !== 'message' || event.message.type !== 'text') {
@@ -77,7 +79,7 @@ function handleEvent(event) {
 }
 
 // listen()メソッドを実行して3000番ポートで待受
-app.listen(PORT);
+//app.listen(PORT);
 // Promiseの詳細なスタックトレースを出力する
-process.on('unhandledRejection', console.dir);
-console.log(`Server running at ${PORT}`);
+//process.on('unhandledRejection', console.dir);
+//console.log(`Server running at ${PORT}`);
