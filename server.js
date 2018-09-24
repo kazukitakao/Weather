f977c365f55ea266edd58b4b89552cf0eca16c81
kazuki takao <kazukitakao55@gmail.com>
2018-09-25 01:15:50 +0900

    rss情報をLINEに送る処理を一旦作成

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const line = require('@line/bot-sdk');
 // XMLパース
 const parseString = require('xml2js').parseString;
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 // LINESECRETとTOKEN
 const config = {
@@ -17,21 +17,63 @@ const config = {
 
 const client = new line.Client(config);
 
-// 3つの関数を作成予定？？
-// rssを取得する関数
-// rssを整形する関数
-// LINEへpushする関数
+function getRSS(url){
+    return new Promise(function(resolve,reject){
+        let request = require('request');
+        console.log('start');
+        request(url, function (err, response, body) {
+            if (!err && response.statusCode == 200) {
+
+                let message = '';
+
+                parseString(body, function (err, obj) {
+                    if (err) { console.log(err); return; }
+            
+                    let date = obj.weatherforecast.pref[0].area[3].info[0].$.date;
+                    let items = obj.weatherforecast.pref[0].area[3].info[0].rainfallchance;
+                    message = '今日の日付：' + date + '\n';
+                    items[0].period.forEach(period => {
+                        message += period.$.hour + '時：' + period._ + '% \n'
+                    });
+                    // message = JSON.stringify({
+                    //     type: 'text',
+                    //     text: message
+                    // });
+                    console.log('取得完了:' + message);
+                    //console.log(message);
+                });
+               resolve(message);
+            }
+        });
+    })
+}
 
 // データ取得先のURLを設定する
 const url = "http://www.drk7.jp/weather/xml/13.xml";
 
-let request = require('request');
+getRSS(url).then(function testMessage(result){
 
-request(url, function (err, response, body) {
-    if (!err && response.statusCode == 200) {
-        analyzeRSS(body);
-    }
+    console.log('非同期テスト:' + result);
+    const message = {
+      type: 'text',
+      text: result
+    };
+    client.pushMessage('U1c6bab7aa9b25288eab65bfd8e4011c1', message)
+        .catch((err) => {
+        console.log(err.message);
+    });
+
+}).catch(function onReject(err){
+    console.log(err.message);
 });
+
+// let request = require('request');
+// request(url, function (err, response, body) {
+//     if (!err && response.statusCode == 200) {
+//         analyzeRSS(body);
+//     }
+// });
+
 
 // ％によって送信するメッセージを設定する
 // 文字列の中に取得した降水確率をFormatする
@@ -45,59 +87,48 @@ function analyzeRSS(rss) {
         let message = '今日の日付：' + date + '\n';
         items[0].period.forEach(period => {
             message += period.$.hour + '時：' + period._ + '% \n'
-            //console.log(period.$.hour + '時：' + period._ + '%');
         });
-        console.log(message);
-        // 取得したデータをjson形式に変換
-        console.log(JSON.stringify({
-            type: 'text',
-            text: message
-        }));
-        return message = JSON.stringify({
-            type: 'text',
-            text: message
-        });
-
+        
+        console.log('取得完了：' + message);
+        //console.log(message);
     });
-}
-
-// プッシュメッセージサンプル
-const message = {
-  type: 'text',
-  text: 'Hello World!'
 };
 
-client.pushMessage('U1c6bab7aa9b25288eab65bfd8e4011c1', message)
-  .catch((err) => {
-      console.log('error');
-    // error handling
-  });
+// プッシュメッセージサンプル
+// const message = {
+//   type: 'text',
+//   text: 'Hello World!'
+// };
+// console.log(message);
+
+// client.pushMessage('U1c6bab7aa9b25288eab65bfd8e4011c1', message)
+//   .catch((err) => {
+//       console.log(err.message);
+// });
 
 
 
+// const app = express();
 
+// app.post('/webhook', line.middleware(config), (req, res) => {
+//     console.log(req.body.events);
+//     // handleEventが完全に完了したら、json形式で返す
+//     Promise
+//         .all(req.body.events.map(handleEvent))
+//         .then((result) => res.json(result));
+// });
 
-const app = express();
+// function handleEvent(event) {
+//     if (event.type !== 'message' || event.message.type !== 'text') {
+//         return Promise.resolve(null);
+//     }
 
-app.post('/webhook', line.middleware(config), (req, res) => {
-    console.log(req.body.events);
-    // handleEventが完全に完了したら、json形式で返す
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
-});
-
-function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
-
-    // テキストメッセージリクエストを行う関数
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: event.message.text // 実際に返信の言葉を入れる箇所
-    });
-}
+//     // テキストメッセージリクエストを行う関数
+//     return client.replyMessage(event.replyToken, {
+//         type: 'text',
+//         text: event.message.text // 実際に返信の言葉を入れる箇所
+//     });
+// }
 
 // listen()メソッドを実行して3000番ポートで待受
 //app.listen(PORT);
