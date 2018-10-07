03d591ce448d5d6202dc949183fe76dacb00e838
kazuki takao <kazukitakao55@gmail.com>
2018-10-07 23:50:41 +0900

    設定ファイルを修正

--- db/post.js
+++ db/post.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-    'postgres://postgres:postgres@localhost/weather',
+    process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/weather',
     {
         logging: false,
         operatorsAliases: false
