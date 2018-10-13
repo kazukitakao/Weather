4170115cfaf98dd0d13e1bdac65b3ab628d3a344
kazuki takao <kazukitakao55@gmail.com>
2018-10-14 03:06:23 +0900

    メッセージの表示形式を修正

--- server.js
+++ server.js
@@ -43,9 +43,13 @@ function getRSS(url){
                                          '今日も一日楽しんでいこうね(^^)', 
                                          '楽しいことがありますように(^^)'];
                     const word3 = '今日は雨が降りそうだから傘を忘れないでね！'
-                    let templeteString = `${sentenceBegin[Math.floor(Math.random() * sentenceBegin.length)]}\n${word3}\n降水確率はこんな感じだよ。\n6〜12時 ${per6to12}%\n12〜18時 ${per12to18}%\n18〜24時 ${per18to24}%\n
+                    let templeteString = `${sentenceBegin[Math.floor(Math.random() * sentenceBegin.length)]}\n
+                                          ${word3}\n降水確率はこんな感じだよ。\n
+                                          6〜12時 ${per6to12}%\n
+                                          12〜18時 ${per12to18}%\n
+                                          18〜24時 ${per18to24}%\n
                                           ${sentenceEnd[Math.floor(Math.random() * sentenceEnd.length)]}`;
-                    message = templeteString;
+                    message = templeteString.trim();
                     // items[0].period 00-06時の降水確率で表示させるか判断
                     // 頭と最後に文字を結合
                     // 降水確率の比較方法 時間は配列
