6d109d742d34bf743cf13b92ec02c9b028e6702b
kazuki takao <kazukitakao55@gmail.com>
2018-10-14 03:33:16 +0900

    メッセージの表示方法を修正

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ function getRSS(url){
                                          '楽しいことがありますように(^^)'];
                     const word3 = '今日は雨が降りそうだから傘を忘れないでね！'
                     // テンプレート文字列は改行がそのまま反映される
-                    let templeteString = `${sentenceBegin[Math.floor(Math.random() * sentenceBegin.length)]} ${word3} 降水確率はこんな感じだよ。
+                    let templeteString = `${sentenceBegin[Math.floor(Math.random() * sentenceBegin.length)]}
+                                          ${word3} 降水確率はこんな感じだよ。
                                           6〜12時 ${per6to12}%\r12〜18時${per12to18}%\r18〜24時 ${per18to24}%
                                           ${sentenceEnd[Math.floor(Math.random() * sentenceEnd.length)]}`;
                     message = templeteString;
